88eab2d210c65b4fc6c88b5f306be0e9ea542b4b
illia pushnov <illia.pushnov@gmail.com>
2020-09-24 15:06:16 +0300

    Initial Commit

---
+++ itaxo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  itaxo
+//
+//  Created by Illia Pushnov on 24.09.2020.
+//  Copyright © 2020 Illia Pushnov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
