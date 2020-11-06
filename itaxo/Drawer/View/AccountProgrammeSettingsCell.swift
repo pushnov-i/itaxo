b96eb1edee37fbe7e68b4fd89f489784cc4a20c8
GenuineMas <MasalitinJW@gmail.com>
2020-11-06 21:07:28 +0200

    creating AccountProgrammeSettingsCell

--- itaxo/Drawer/View/AccountProgrammeSettingsCell.swift
+++ itaxo/Drawer/View/AccountProgrammeSettingsCell.swift
@@ -5,7 +5,69 @@
 //  Created by Genuine on 06.11.2020.
 //  Copyright © 2020 Illia Pushnov. All rights reserved.
 //
+import UIKit
+
 
 class AccountProgrammeSettingsCell {
+//    let cityLabel : UILabel = {
+//        
+//    }
     
+    let cityLabel: UILabel = {
+        let iconView = UILabel()
+        iconView.contentMode = .scaleAspectFit
+        iconView.clipsToBounds = true
+        return iconView
+    }()
+    
+    let cityCellTextField: UITextField =
+    {
+        let cellTextField = UITextField()
+        cellTextField.autocapitalizationType = .none
+        cellTextField.layer.cornerRadius = 16.0
+        cellTextField.layer.borderColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0).cgColor
+        cellTextField.layer.borderWidth = 1.0
+        cellTextField.textContentType = .emailAddress
+        cellTextField.layer.masksToBounds = true
+        cellTextField.font = .systemFont(ofSize: 16.0, weight: .regular)
+        cellTextField.placeholder = "Ваш е-мейл"
+        cellTextField.textContentType = .emailAddress
+        return cellTextField
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        backgroundColor = .black
+        addSubview(iconImageView)
+        cityLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.left.equalTo(12)
+            make.height.equalTo(18)
+            make.width.equalTo(41)
+            
+        }
+        
+        addSubview(cellTextField)
+        cityCellTextField.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.left.equalTo(12)
+            make.height.equalTo(18)
+            make.width.equalTo(41)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(withViewModel viewModel: SettingsAccountMenuDelegate) {
+       
+        
+        cityCellTextField.placeholder = viewModel.placeholder
+        cityLabel.image = UIImage(named:viewModel.image!)
+        
+        
+    }
 }
--- itaxo/Drawer/View/AccountSettingsCell.swift
+++ itaxo/Drawer/View/AccountSettingsCell.swift
@@ -17,26 +17,25 @@ class AccountSettingsCell: UITableViewCell {
         return iconView
     }()
     
-    let emailTextField: UITextField =
+    let accountCellTextField: UITextField =
     {
-        let emailTextField = UITextField()
-        emailTextField.autocapitalizationType = .none
-        emailTextField.layer.cornerRadius = 16.0
-        emailTextField.layer.borderColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0).cgColor
-        emailTextField.layer.borderWidth = 1.0
-        emailTextField.textContentType = .emailAddress
-        emailTextField.layer.masksToBounds = true
-        emailTextField.font = .systemFont(ofSize: 16.0, weight: .regular)
-        emailTextField.placeholder = "Ваш е-мейл"
-        emailTextField.textContentType = .emailAddress
-        return emailTextField
+        let cellTextField = UITextField()
+        cellTextField.autocapitalizationType = .none
+        cellTextField.layer.cornerRadius = 16.0
+        cellTextField.layer.borderColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0).cgColor
+        cellTextField.layer.borderWidth = 1.0
+        cellTextField.textContentType = .emailAddress
+        cellTextField.layer.masksToBounds = true
+        cellTextField.font = .systemFont(ofSize: 16.0, weight: .regular)
+        cellTextField.textContentType = .emailAddress
+        return cellTextField
     }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
-        backgroundColor = .white
+        backgroundColor = .black
         addSubview(iconImageView)
         iconImageView.snp.makeConstraints { make in
             make.centerY.equalToSuperview()
@@ -46,8 +45,8 @@ class AccountSettingsCell: UITableViewCell {
             
         }
         
-        addSubview(emailTextField)
-        emailTextField.snp.makeConstraints { make in
+        addSubview(accountCellTextField)
+        accountCellTextField.snp.makeConstraints { make in
             make.centerY.equalToSuperview()
             make.left.equalTo(12)
         }
@@ -60,7 +59,7 @@ class AccountSettingsCell: UITableViewCell {
     func configure(withViewModel viewModel: SettingsAccountMenuDelegate) {
        
         
-        emailTextField.placeholder = viewModel.placeholder
+        accountCellTextField.placeholder = viewModel.placeholder
         iconImageView.image = UIImage(named:viewModel.image!)
         
         
