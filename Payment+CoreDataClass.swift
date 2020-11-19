16ffbb5c50d122c4952fa2e55553b477ad4e3766
GenuineMas <MasalitinJW@gmail.com>
2020-11-19 11:55:32 +0200

    add relationships

---
+++ Payment+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Payment+CoreDataClass.swift
+//  itaxo
+//
+//  Created by Genuine on 19.11.2020.
+//  Copyright © 2020 Illia Pushnov. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Payment)
+public class Payment: NSManagedObject {
+
+}
